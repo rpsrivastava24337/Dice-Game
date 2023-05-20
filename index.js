6c4e146e5fa9f36c529e2c313b377a615f8e03dd
Ravi Prakash Srivastava <123312729+rpsrivastava24337@users.noreply.github.com>
2023-05-20 09:57:47 +0530

    Update index.js
     fix - increment and decrement

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var diceNumber2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", diceNumber2);
 
-if (randomNumber1 > randomNumber2) {
+if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 2 win ";
-} else if (randomNumber1 < randomNumber2) {
+} else if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 win ";
 } else {
   document.querySelector("h1").innerHTML = "Draw ";
